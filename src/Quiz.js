f8534612e7286e3170dfedcdd551fc5f51c94845
Richard Evans <evansr@Bma.Local>
2020-01-06 15:36:09 +0000

    Display Error when wrong answer

--- src/Quiz.js
+++ src/Quiz.js
@@ -22,7 +22,7 @@ class Quiz extends Component {
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> : 
-                    <QuizQuestion class="QuizQuestion" quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
+                    <QuizQuestion className="QuizQuestion" quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
                                   showNextQuestionHandler={this.showNextQuestion.bind(this)} /> }
             </div>
         )
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {incorrectAnswer: false}
+    }
+    
     handleClick(buttonText) {
         if(this.props.quiz_question.answer === buttonText) {
+            this.setState(() => {
+                return {incorrectAnswer: false }
+            })
             this.props.showNextQuestionHandler();
+        } else {
+            this.setState(() => {
+                return {incorrectAnswer: true }
+            })
         }
     }
     
@@ -20,6 +32,7 @@ class QuizQuestion extends Component {
                     })}
                 </ul>
             </section>
+            {(this.state.incorrectAnswer) ? <p className='error'>Sorry, that's not right</p> : null}
         </main>
     }
 }
